f528d2a3050f3b6130cd8cc54d0b0cff6344d8e0
Lục Anh <120559330+luctananh@users.noreply.github.com>
2026-04-06 12:47:11 +0700

    remove duplicate appfont subfolder

--- appfont/web/api.js
+++
@@ -1,5 +0,0 @@
-// Sets up the API client for interacting with your backend. 
-// For your API reference, visit: https://docs.gadget.dev/api/appfont
-import { Client } from "@gadget-client/appfont";
-
-export const api = new Client({ environment: window.gadgetConfig.environment });
